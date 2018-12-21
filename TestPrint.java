40cd4172b6d312c223835c5e1856e78f58a6f7ea
rahulahire7 <43874308+rahulahire7@users.noreply.github.com>
2018-12-21 13:56:00 +0530

    First Program

---
+++ TestPrint.java
@@ -0,0 +1,16 @@
+public class TestPrint {
+	public static void main(String[] args) {
+		Welcome w=new Welcome();
+		w.printText("Hello Java");
+		
+		w.printText("Hello Ad.Java");
+		w.printText("Hello Friends");
+		
+		w.printText("Good Bye");
+		
+		w.printText("All the best");
+		
+
+	}
+
+}
---
+++ Welcome.java
@@ -0,0 +1,5 @@
+public class Welcome {
+	public void printText(String str){
+		System.out.println(str);
+	}
+}
